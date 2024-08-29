e04371b668959d2364d0ea3b197d612287f0acf0
Tonia Hardeman <tonia.hardeman@gmail.com>
2024-08-29 13:01:48 -0400

    Update contact.php
    Fixed the contact form code.

--- contact.php
+++ contact.php
@@ -1,14 +1,26 @@
 <?php
 $to = 'hello@toniahardeman.com';
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+// Validate and sanitize input
+if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-$subject = 'Contact Form Submission';
-$body = "Name: $name\nEmail: $email\nMessage: $message";
+    if ($name && $email && $message) {
+        $subject = 'Contact Form Submission';
+        $body = "Name: $name\nEmail: $email\nMessage: $message";
 
-mail($to, $subject, $body);
-
-header('Location: https://toniahardeman.com/thanks.html');
-exit;
+        // Send email
+        if (mail($to, $subject, $body)) {
+            header('Location: https://toniahardeman.com/thanks.html');
+            exit;
+        } else {
+            echo 'Error sending email. Please try again later.';
+        }
+    } else {
+        echo 'Please fill in all the required fields.';
+    }
+} else {
+    echo 'Invalid request. Please try again.';
+}
